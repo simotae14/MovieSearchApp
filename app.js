75373e5cfddcd0ac1e2d0ed9cad197dd3509bcea
Simone Taeggi <startae14@gmail.com>
2017-03-18 09:39:10 +0000

    aggiunta ricerca

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+// setto le view engine con ejs
+app.set("view engine", "ejs");
+
+// creo la route per la ricerca
+app.get("/", function(req, res){
+  res.render("search");
+});
 
 // creo route dei risultato
 app.get("/results", function(req, res){
-  request("http://omdbapi.com?s=california", function(error, response, body){
+  request("http://omdbapi.com?s=iowa", function(error, response, body){
     if(!error && response.statusCode == 200) {
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]["Title"]);
+      var data = JSON.parse(body);
+      res.render("results", { data: data });
     }
   });
 });
